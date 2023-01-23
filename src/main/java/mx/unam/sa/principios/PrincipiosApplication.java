99d656747ba43e7f306bbc593ecbee8162c9f6fc
miguelangel <miguelangel@dgp.unam.mx>
2023-01-23 12:05:21 -0600

    Inicio

---
+++ src/main/java/mx/unam/sa/principios/PrincipiosApplication.java
@@ -0,0 +1,13 @@
+package mx.unam.sa.principios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrincipiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrincipiosApplication.class, args);
+	}
+
+}
